3cfc838854c598e0a092b29d4dbcd674017d92ae
yash3001 <yashchauhan197@gmail.com>
2020-08-08 19:31:21 +0530

    some minor changes after barpadding

--- config.def.h
+++ config.def.h
@@ -3,12 +3,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 5;       /* vertical padding of bar */
+static const int sidepad            = 12;       /* horizontal padding of bar */
 static const char *fonts[]          = { "mononoki:size=12", "fontawesome:size=12" };
 static const char dmenufont[]       = "mononoki:size=12";
 static const char col_gray1[]       = "#222222";
