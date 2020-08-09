fc045f58e8208f7988fe045827243ba8790bdb1f
yash3001 <yashchauhan197@gmail.com>
2020-08-09 20:13:37 +0530

    some minor changes

--- dwmblock_scripts/battery.sh
+++ dwmblock_scripts/battery.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-capacity="$(cat /sys/class/power_supply/BAT1/capacity)% "
-status=$(sed "s/[Dd]ischarging//;s/[Cc]harging//;s/[Ff]ull//" /sys/class/power_supply/BAT1/status)
-
-printf %s%s%% " $status " "$capacity"
+capacity="$(cat /sys/class/power_supply/BAT1/capacity)%"
+#status=$(sed "s/[Dd]ischarging//;s/[Cc]harging//;s/[Ff]ull//" /sys/class/power_supply/BAT1/status)
+status=$(sed "s/[Dd]ischarging/d/;s/[Cc]harging//;s/[Ff]ull/d/" /sys/class/power_supply/BAT1/status)
+if [[ ($status == "d") ]]
+then
+    if [[ ${capacity:0:-1} -gt "90" ]]
+    then
+        status=""
+    elif [[ ${capacity:0:-1} -gt "75" ]]
+    then 
+        status=""
+    elif [[ ${capacity:0:-1} -gt "50" ]]
+    then 
+        status=""
+    elif [[ ${capacity:0:-1} -gt "35" ]]
+    then 
+        status=""
+    else
+        status=""
+    fi
+fi
+printf %s%s%% " $status " "$capacity "
