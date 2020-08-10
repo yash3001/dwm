2b27912bc5b1fceac3a638401a7534888b0310f0
yash3001 <yashchauhan197@gmail.com>
2020-08-10 18:29:07 +0530

    played with dmenu

--- config.def.h
+++ config.def.h
@@ -84,7 +84,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", "30", NULL };
+static const char *dmenucmd[]  = { "dmenu_run", "-h", "40", "-c", "-l", "10", NULL };
 static const char *termcmd[]   = { "terminator", NULL };
 static const char *stcmd[]     = { "st", NULL };
 static const char *firefox[]   = { "firefox", NULL };
