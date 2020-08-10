6631c40373a6c03bcf707542e6ec5e88fde0a94d
yash3001 <yashchauhan197@gmail.com>
2020-08-10 21:53:15 +0530

    added dwmblocks folder in dwm

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,26 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	//{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
+	
+	//{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	//{"", "~/bin/statusbar/volume",						0,		10},
+
+	//{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
+	
+	//{"", "~/bin/statusbar/battery",						5,		0},
+
+	//{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+
+	//{"", "~/bin/statusbar/clock",						5,		0},
+
+    {"", "/home/yash/.config/dwm/dwmblock_scripts/sound.sh", 0, 11},
+
+    {"", "/home/yash/.config/dwm/dwmblock_scripts/battery.sh", 10, 0},
+
+    {"", "/home/yash/.config/dwm/dwmblock_scripts/time.sh", 60, 0}
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
