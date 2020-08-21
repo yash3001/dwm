8fff21cccb275dc0b4d579d1cd7ea77b9c8f2912
yash3001 <yashchauhan197@gmail.com>
2020-08-21 21:14:13 +0530

    some minor changes

--- dwmblocks/dwmblock_scripts/netspeed.sh
+++ dwmblocks/dwmblock_scripts/netspeed.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-speed=$(/home/yash/.config/dwm/dwmblocks/dwmblock_scripts/netspeed.py -d "wlo1" -t "0.5" | awk 'OFS="   " {print $1, $2}')
+speed=$(/home/yash/.config/dwm/dwmblocks/dwmblock_scripts/netspeed.py -d "wlo1" -t "1" | awk 'OFS="   " {print $1, $2}')
 icon=''
 printf " %s %s \\n" "$icon" "$speed"
